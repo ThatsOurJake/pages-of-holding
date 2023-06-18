66204f6f9ff925cf55617fbb2e16875d4488e20a
Jake <jakeking@hotmail.co.uk>
2023-06-18 17:10:08 +0100

    Drone build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ['s.gravatar.com']
-  }
+  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
